ce2b46c9891f37dae67386e4327016e7230cc522
Zack OConnor <zackary25@gmail.com>
2025-03-13 11:32:10 -0400

    Added an object to store projects and the main screen now updates the project sections using object data.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
+const projectData = {
+    websites: [
+        {
+            title: `Blackjack`,
+            imagePath: "Assets/Screen Shot 2025-03-10 at 5.31.43 PM.png",
+            description: "this is a blackjack game."
+        }, {
+            title: "Tamagotchi",
+            imagePath: "Assets/Screen Shot 2025-03-12 at 3.42.30 PM.png",
+            description: "this is a tamagotchi game."
+        }
+    ],
+
+    apps: [
+        {
+            title: `ios app`,
+            imagePath: "Assets/Screen Shot 2025-03-10 at 5.31.43 PM.png",
+            description: "this is an app."
+        }
+    ]
+
+};
+
+
 const hiddenElements = document.querySelectorAll(`.hidden`);
 const projectElements = document.querySelectorAll(`.project`);
+const projectImageElement = document.querySelector(`.project-image`);
 const modalElement = document.querySelector(`.modal`);
 const closeModalButton = document.querySelector(`.close-modal-button`);
 const sendButton = document.querySelector(`.send-button`);
 const githubButton = document.querySelector(`.social-buttons`);
+
+
 
 
 // User interaction 1: animation on scroll
@@ -18,7 +45,28 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((element) => observer.observe(element));
+
+
+
+
+// dynamically update projects section
+const updateProjectSection = (classSection, projects) => {
+    const projectsContainer = document.querySelector(`.${classSection} .projects-container`);
+
+    projects.forEach(project => {
+        const newProjectElement = document.createElement(`div`);
+        newProjectElement.classList.add(`project`);
+
+        newProjectElement.innerHTML = `<img class="project-image" src="${project.imagePath}" alt="Image of really cool project!">`;
+        projectsContainer.appendChild(newProjectElement);
+    })
+}
+
+updateProjectSection(`websites`, projectData.websites);
+updateProjectSection(`apps`, projectData.apps);
+
+
 
 
 // User interaction 2 & 3: mouse hover animation and display modal on click
@@ -34,13 +82,14 @@ projectElements.forEach(project => {
 
     project.addEventListener(`click`, () => {
         modalElement.classList.add(`show`);
-
     })
 })
 
 closeModalButton.addEventListener(`click`, () => {
     modalElement.classList.remove(`show`);
 });
+
+
 
 
 // User interaction 4: input field validation
