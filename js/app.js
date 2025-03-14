7c24f3001bbc3509390021cf2e893f1db7926cb0
Zack OConnor <zackary25@gmail.com>
2025-03-14 09:52:42 -0400

    Added a functional light / dark mode toggle.

--- js/app.js
+++ js/app.js
@@ -3,35 +3,40 @@ const projectData = {
         {
             title: `Blackjack`,
             imagePath: "Assets/Screen Shot 2025-03-10 at 5.31.43 PM.png",
-            description: "this is a blackjack game."
+            description: `this is a simplified blackjack game. Created by Dean, Conner, Ayah, and I.`
         }, {
-            title: "Tamagotchi",
-            imagePath: "Assets/Screen Shot 2025-03-12 at 3.42.30 PM.png",
-            description: "this is a tamagotchi game."
+            title: `Tamagotchi`,
+            imagePath: `Assets/Screen Shot 2025-03-12 at 3.42.30 PM.png`,
+            description: `Simple tamagotchi game.`
         }, {
-            title: "Tamagotchi",
-            imagePath: "Assets/Screen Shot 2025-03-12 at 3.42.30 PM.png",
-            description: "this is a tamagotchi game."
-        }, {
-            title: "Tamagotchi",
-            imagePath: "Assets/Screen Shot 2025-03-12 at 3.42.30 PM.png",
-            description: "this is a tamagotchi game.asfasdfasdfasdfasdfasdfasfasdfasdffasdfljhaldfjkhasdhflakjshdflkjahsdlfahsldfhaskdjhflkasjdhflkjsadhflksjhdflksjf"
+            title: `iOS Portfolio`,
+            imagePath: `Assets/Screen Shot 2025-03-13 at 8.43.31 PM.png`,
+            description: `My first website from 5 years ago.`
         }
     ],
 
     apps: [
         {
-            title: `ios app`,
-            imagePath: "Assets/Screen Shot 2025-03-10 at 5.31.43 PM.png",
-            description: "this is an app."
+            title: `DVD's`,
+            imagePath: "Assets/Screen Shot 2025-03-13 at 8.26.33 PM.png",
+            description: `This is an app that allows you to keep track of the movies(tv shows coming soon) you own on DVD or Blue-ray. I am using "TMDB" api for the movie data.`
+        }, {
+            title: `Hours`,
+            imagePath: "Assets/Screen Shot 2025-03-13 at 9.11.08 PM.png",
+            description: `A simple way to track your work hours.`
+        }, {
+            title: `Facebook`,
+            imagePath: "Assets/Screen Shot 2025-03-13 at 9.11.18 PM.png",
+            description: `Facebook UI with working like button.`
         }
+
     ]
 
 };
 
 
-
-
+const lightDarkModeToggle = document.getElementById(`light-dark-mode-toggle`);
+const body = document.body
 const hiddenElements = document.querySelectorAll(`.hidden`);
 const projectImageElement = document.querySelector(`.project-image`);
 const modalElement = document.querySelector(`.modal`);
@@ -42,7 +47,17 @@ const githubButton = document.querySelector(`.social-buttons`);
 
 
 
-// User interaction 1: animation on scroll
+// User interaction 1: light / dark mode toggle
+lightDarkModeToggle.addEventListener(`click`, () => {
+    body.classList.toggle(`light-mode`);
+    body.classList.toggle(`dark-mode`);
+    console.log(body.classList);
+});
+
+
+
+
+// User interaction 2: animation on scroll
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -94,7 +109,7 @@ const displayModal = (project) => {
 
 
 
-// User interaction 2 & 3: mouse hover animation and display modal on click
+// User interaction 3 & 4: mouse hover animation and display modal on click
 const projectElements = document.querySelectorAll(`.project`);
 projectElements.forEach(project => {
     project.addEventListener(`mouseover`, () => {
@@ -126,7 +141,7 @@ updateProjectSection(`apps`, projectData.apps);
 
 
 
-// User interaction 4: input field validation
+// User interaction 5: input field validation
 sendButton.addEventListener(`click`, event => {
     let name = document.querySelector(`#name`).value;
     let email = document.querySelector(`#email`).value;
